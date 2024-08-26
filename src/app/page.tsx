17c23db9f7a18172f272dcb84dc43ea965e01303
GamerCleanVic <jonassd2000@gmail.com>
2024-08-26 20:59:43 -0300

    +rails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -398,6 +398,23 @@ export default function Home() {
           </p>
         </a>
 
+        <a
+          href="https://www.youtube.com/playlist?list=PLe3LRfCs4go-mkvHRMSXEOG-HDbzesyaP"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            RUBY ON RAILS{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          RAILS P/ INICIANTES.
+          </p>
+        </a>
+
       </div>
 
     </main>
