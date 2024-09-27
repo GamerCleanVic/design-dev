6731bdb14185dfbd01ed233063632a0cf0cff5d5
GamerCleanVic <jonassd2000@gmail.com>
2024-09-27 19:45:33 -0300

    PHP + Laravel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -327,40 +327,6 @@ export default function Home() {
           </a>
 
         <a
-          href="https://alunos.b7web.com.br/curso/laravel/laravel9-introducao-ao-curso"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 border card-buttom"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            LARAVEL{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            B7WEB - LARAVEL.
-          </p>
-        </a>
-
-        <a
-          href="https://alunos.b7web.com.br/curso/php/php83-introducao"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            PHP{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            B7WEB - PHP.
-          </p>
-        </a>
-
-        <a
           href="https://www.udemy.com/course-dashboard-redirect/?course_id=3803130"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
           target="_blank"
@@ -752,6 +718,40 @@ export default function Home() {
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
           B7WEB - NEXT.JS.
+          </p>
+        </a>
+
+        <a
+          href="https://alunos.b7web.com.br/curso/php"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            PHP{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            B7WEB - PHP.
+          </p>
+        </a>
+
+        <a
+          href="https://alunos.b7web.com.br/curso/laravel"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 border card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            LARAVEL{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            B7WEB - LARAVEL.
           </p>
         </a>
 
