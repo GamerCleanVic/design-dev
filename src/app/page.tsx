f0a91d43c8c3d602d1327add90fb51488987e3cd
GamerCleanVic <jonassd2000@gmail.com>
2024-11-01 09:20:31 -0300

    Informática Para Concursos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,6 +110,23 @@ export default function Home() {
           </p>
         </a>
 
+        <a
+          href="https://www.udemy.com/course-dashboard-redirect/?course_id=5560950"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+          INFORMÁTICA{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Noções de Informática para Concursos 2023.
+          </p>
+        </a>
+
         </div>
 
       <div className="text-2xl mt-3">TECNOLOGIAS EXTRAS</div>
