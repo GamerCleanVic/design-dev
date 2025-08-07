e1f9d8f77e574c97f1802c7609aeb4a0eff6b5b0
GamerCleanVic <jonassd2000@gmail.com>
2025-08-06 23:38:24 -0500

    DSA Java

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -944,6 +944,23 @@ export default function Home() {
           </p>
         </a>
 
+        <a
+          href="https://youtube.com/playlist?list=PLGxZ4Rq3BOBrgumpzz-l8kFMw2DLERdxi"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 card-buttom"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            DSA COM JAVA{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Loiane Groner - Estrutura de Dados e Algoritmos com Java.
+          </p>
+        </a>
+
         </div>
     </main>
   );
